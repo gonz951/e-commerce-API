b42854983d9e4d40e024e815494a42345f65be06
Fel <felgonzalez951@gmail.com>
2024-05-29 15:54:24 -0600

    finished content for both get routes, edited code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,16 +11,6 @@ router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
-      attributes: {
-        include: [
-          [
-            sequelize.literal(
-              '(SELECT '// ! FINISH LATER
-            ),
-            'allProducts'
-          ],
-        ],
-      },
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -33,19 +23,15 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk({
+    const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
-      attributes: {
-        include: [
-          [
-            sequelize.literal(
-              '(SELECT ' // ! FINISH LATER 
-            ),
-            'singleProduct'
-          ],
-        ],
-      },
     });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No products found with that id.' });
+      return;
+    }
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);

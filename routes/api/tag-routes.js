039314a8be8257f76530abccc1fca634c3fc4e4d
Fel <felgonzalez951@gmail.com>
2024-05-29 19:53:36 -0600

    finished PUT route, tested everythign successfully

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-// TODO: PUT route
+// TODO: FINISHED & TESTED !
 
 router.get('/', async (req, res) => {
   // find all tags
@@ -49,8 +49,17 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update()
-    // ! UNFINISHED
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id.' });
+      return;
+    }
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -63,7 +72,7 @@ router.delete('/:id', async (req, res) => {
       where: { id: req.params.id }
     });
     if (!tagData) {
-      res.status(404).json({ message: 'No tag found with this id.'});
+      res.status(404).json({ message: 'No tag found with this id.' });
       return;
     }
     res.status(200).json(tagData);

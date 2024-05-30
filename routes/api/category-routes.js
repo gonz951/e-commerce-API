388de939da9e78829d44be3e26e0176632f525f3
Fel <felgonzalez951@gmail.com>
2024-05-29 19:42:04 -0600

    finished up and tested PUT and DELETE routes; successful

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-// TODO: POST route, PUT route, DELETE route
 
 router.get('/', async (req, res) => {
   // find all categories
@@ -43,15 +42,42 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if (!categoryData) {
+      res.status(200).json({ message: 'No category found with this id.' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id.' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
